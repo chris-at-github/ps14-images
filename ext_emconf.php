c931d2a41af65040ca28e150328b9c1685a71c83
Christian Pschorr <pschorr.christian@gmail.com>
2024-11-19 22:43:25 +0100

    Initialer Commit nach Erstellung (Kopie von Modulor)

---
+++ ext_emconf.php
@@ -0,0 +1,22 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+	'title' => 'Ps14 Images',
+	'description' => 'Module to extend the TYPO3 Core image module',
+	'category' => 'distribution',
+	'author' => 'Christian Pschorr',
+	'author_email' => 'pschorr.christian@gmail.com',
+	'author_company' => '',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'clearCacheOnLoad' => 0,
+	'version' => '1.0.0',
+	'constraints' => [
+		'depends' => [
+			'typo3' => '12.0.0-12.4.99',
+			'ps14_foundation' => '2.0.0-2.9.99',
+		],
+		'conflicts' => [],
+		'suggests' => [],
+	],
+];
